71b6e57b6ee9a20fcc4cf80c9584ab0845011964
bhargavanarnedra <bhargava@moneyhop.co>
2022-04-26 11:11:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Form, Formik } from "formik";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [isSecond, setissecond] = useState(false);
+  const [isAlluploaded, setisAlluploaded] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        front: null,
+        back: null,
+      }}
+      onSubmit={(values, props) => {
+        let data = new FormData();
+
+        data.append("front", values.front);
+        data.append("back", values.back);
+
+        console.log(values.front);
+        console.log(values.back);
+        axios
+          .post("http://localhost:8080/getSizes", data, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              "Access-Control-Allow-Origin": "*",
+            },
+          })
+          .then((response) => {
+            console.log(response);
+            setissecond(false);
+            setisAlluploaded(false);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        props.setSubmitting(false);
+      }}
+    >
+      {(formik) => {
+        return (
+          <>
+            <Form>
+              <input
+                id="filefront"
+                name="front"
+                type="file"
+                onChange={(event) => {
+                  const files = event.target.files;
+                  let myFiles = Array.from(files);
+                  formik.setFieldValue("front", myFiles[0]);
+                  setissecond(true);
+                }}
+              />
+
+              {isSecond && (
+                <input
+                  id="fileback"
+                  name="back"
+                  type="file"
+                  onChange={(event) => {
+                    const files = event.target.files;
+                    let myFiles = Array.from(files);
+                    formik.setFieldValue("back", myFiles[0]);
+                    setisAlluploaded(true);
+                  }}
+                />
+              )}
+              {isAlluploaded && (
+                <button type="submit" disabled={formik.isSubmitting}>
+                  Submit
+                </button>
+              )}
+            </Form>
+          </>
+        );
+      }}
+    </Formik>
   );
 }
 
